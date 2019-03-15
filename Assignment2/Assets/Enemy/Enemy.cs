493e13c607a52e82d9854e115026f923a6c45f80
danny-to7 <danny.to@uoit.net>
2019-03-14 23:41:23 -0400

    Basic shooter

---
+++ Assignment2/Assets/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] public int hp = 100;
+
+    void ReceiveHit()
+    {
+        hp -= 10;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assignment2/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerShoot : MonoBehaviour {
+    [SerializeField] private Transform camera = null;
+
+    [SerializeField] private ParticleSystem terrainImpactEffect = null;
+    [SerializeField] private ParticleSystem enemyImpactEffect = null;
+    [SerializeField] private Text score = null;
+
+    private float range = 100f;
+    private int hitCount = 0;
+
+    void Update() {
+        if (Input.GetButtonDown("Shoot")) {
+            RaycastHit hit;
+
+            LayerMask enemyMask = LayerMask.GetMask("Enemies");
+            LayerMask wallsMask = LayerMask.GetMask("GameArea");
+
+            if (Physics.Raycast(camera.position, camera.forward, out hit, range, enemyMask)) {
+                Debug.Log("Shot enemy: " + hit.collider.name + " at " + hit.point);
+                ParticleSystem sparks = Instantiate(enemyImpactEffect, hit.point, Quaternion.identity);
+                hit.transform.SendMessageUpwards("ReceiveHit");
+                hitCount += 1;
+                score.text = hitCount.ToString();
+
+            } else if (Physics.Raycast(camera.position, camera.forward, out hit, range, wallsMask)) {
+                Debug.Log("Shot the wall: " + hit.collider.name);
+                ParticleSystem dirtKick = Instantiate(terrainImpactEffect, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
